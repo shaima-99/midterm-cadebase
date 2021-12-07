0931911f2d66ae1fc17998d30288f46ca36b0530
shaima-99 <shaima.husein@gmail.com>
2021-12-07 13:38:21 +0300

    i have added title and completed to note class

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,3 +1,10 @@
 //! Define the model class Note here
 
-class Note {}
+class Note {
+  String title;
+  bool completed;
+
+  Note({ this.title, this.completed = false});
+  Note.copy(Note from) : this(title: from.title, completed: from.completed);
+}
+ 
